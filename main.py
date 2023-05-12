b2f7331b895f18da5c8c75b522ec69e465769421
Cvs Enes <117417788+Cvsns@users.noreply.github.com>
2023-05-13 01:34:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+while True:
+    user = input("Veuillez choisir pierre, feuille ou ciseaux : ").lower()
+    print("Vous avez fait votre choix.")
+    pc = random.choice(["pierre", "feuille", "ciseaux"])
+    print("L'ordinateur a fait son choix.")
+    if user == pc:
+        print("Égalité !")
+    elif (user == "pierre" and pc == "ciseaux") \
+            or (user == "feuille" and pc == "pierre") \
+            or (user == "ciseaux" and pc == "feuille"):
+        print("Vous avez gagné !")
+    else:
+        print("Vous avez perdu !")
+    reponse = input("Voulez-vous rejouer ? (oui/non) : ")
+    if reponse.lower() not in ["oui", "o"]:
+        break
